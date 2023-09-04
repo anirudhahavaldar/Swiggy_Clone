8f9ddf21f00b6e61233845b1d276fc3f44a2420a
Anirudha Havaldar <anirudhahavaldar@gmail.com>
2023-09-04 20:44:12 +0530

    Episode 03 Laying the Foundation

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const div = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', {}, 'I am h1 tag'),
-    React.createElement('h2', {}, 'I am h2 tag'),
-  ]),
-  React.createElement('div', { id: 'child2' }, [
-    React.createElement('h1', {}, 'I am h1 tag'),
-    React.createElement('h2', {}, 'I am h2 tag'),
-  ]),
-]);
+const jsxHeading = (
+  <div>
+    <h1>Namaste React</h1>
+  </div>
+);
+console.log(jsxHeading);
+
+const HeadingComponent = () => (
+  <div id="container">
+    <h1>This is React Functional Component</h1>
+  </div>
+);
+
+const name = <h1>Anirudha K H</h1>;
+
+const Greeting = () => (
+  <div>
+    <h1>Hello {name}, Welcome!!!!</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(div);
+root.render(<Greeting />);
