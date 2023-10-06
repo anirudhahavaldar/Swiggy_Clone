e12331f10a758d97e802f348f3af24d5cf35600f
Anirudha Havaldar <anirudhahavaldar@gmail.com>
2023-10-06 23:38:35 +0530

    partially completed Episode 06 Explore the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,43 @@
-import { useState } from 'react';
-import { restaurants } from '../utils/mockData';
+import { useEffect, useState } from 'react';
 import RestaurantCard from './RestaurantCard';
+import Shimmer from './Shimmer';
 
 export const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    let data = fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+    );
+
+    const jsonData = await data.json();
+
+    setListOfRestaurants(
+      jsonData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return (
+      <>
+        <Shimmer />;
+        <Shimmer />;
+        <Shimmer />;
+        <Shimmer />;
+        <Shimmer />;
+        <Shimmer />;
+        <Shimmer />;
+        <Shimmer />;
+        <Shimmer />;
+        <Shimmer />;
+      </>
+    );
+  }
 
   return (
     <div>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Shimmer = () => {
+  return <div className="shimmer"></div>;
+};
+
+export default Shimmer;
